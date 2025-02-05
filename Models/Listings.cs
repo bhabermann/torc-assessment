5d27cd4e4885945e874f8019232e1feb08f72f67
Bruno Habermann <dev@habermann.tec>
2025-02-05 19:56:25 -0300

    feat(model): add data validations to Listing model

--- Models/Listings.cs
+++ Models/Listings.cs
@@ -1,10 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RealEstateListingApi.Models
 {
     public class Listing
     {
-    public string Id { get; set; } = string.Empty;  // Default to empty string if nulls aren't allowed
-    public string Title { get; set; } = string.Empty;
-    public decimal Price { get; set; }  // Decimal is a value type and non-nullable by default
-    public string? Description { get; set; }  // Mark as nullable if appropriate
-}
+        [Required]
+        [StringLength(36)]
+        public string Id { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [StringLength(2000)]
+        public string? Description { get; set; }
+    }
 }
